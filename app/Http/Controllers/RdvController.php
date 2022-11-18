10b7764c914b7ee5c119ed41e05e6f9e0097cb26
William Mouton <mouton.w2000@gmail.com>
2022-11-18 15:42:09 +0100

    Affichage des rdv : Update RdvController et rdv.blade.php

--- app/Http/Controllers/RdvController.php
+++ app/Http/Controllers/RdvController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RendezVous;
 use Illuminate\Http\Request;
 
 class RdvController extends Controller
 {
     public function index()
     {
-        return view('rdv.rdv');
+        $datas = RendezVous::all();
+        return view('rdv.rdv',['rendezvous'=>$datas]);
     }
+
 }
--- resources/views/rdv/rdv.blade.php
+++ resources/views/rdv/rdv.blade.php
@@ -4,4 +4,17 @@
     <div class="container">
         <p>Ceci est la page de prise des rendez-vous du site.</p>
     </div>
+    <h1>Liste des rendez-vous</h1>
+    <table>
+        <td>ID</td>
+        <td>daterdv</td>
+        <td>heurerdv</td>
+        @foreach ($rendezvous as $rdv)
+        <tr>
+            <td>{{$rdv['id']}}</td>
+            <td>{{$rdv['daterdv']}}</td>
+            <td>{{$rdv['heurerdv']}}</td>
+        </tr>
+        @endforeach
+    </table>
 @endsection
