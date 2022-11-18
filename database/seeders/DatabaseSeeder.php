198e56db1d08f6ddd46dda513a08678b37ff3537
William Mouton <mouton.w2000@gmail.com>
2022-11-18 16:16:46 +0100

    oups virgules

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            PrestationSeeder::class
-            ForfaitSeeder::class
-            RendezVousSeeder::class
+            PrestationSeeder::class,
+            ForfaitSeeder::class,
+            RendezVousSeeder::class,
         ]);
     }
 }
