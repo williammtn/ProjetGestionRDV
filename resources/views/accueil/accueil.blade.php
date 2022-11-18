8d4a8b08dfb66aab80c96eaee9a2ebb8dac91feb
Lucie <lucie_caudron@ens.univ-artois.fr>
2022-11-18 15:18:41 +0100

    Design: Fixs

--- resources/views/accueil/accueil.blade.php
+++ resources/views/accueil/accueil.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div id="carousel"></div>
-    <div class="container">
+    <div class="container py-4">
         <p>Ceci est l'accueil du site.</p>
     </div>
 @endsection
